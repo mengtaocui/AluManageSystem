26e599f157894d49196ba1a3f2f62a0cc3b4506e
mengtaocui <mengtao123>
2017-11-07 14:55:59 +0800

    删除无用模块

--- src/main/java/test/JeecgOneGUI.java
+++ src/main/java/test/JeecgOneGUI.java
@@ -4,13 +4,6 @@ import org.jeecgframework.codegenerate.window.CodeWindow;
 
 import java.sql.SQLOutput;
 
-
-/**
- * 【单表模型】代码生成器入口
- * @author 张代浩
- * @site www.jeecg.org
- *
- */
 public class JeecgOneGUI {
 
 	public static void main(String[] args) {
