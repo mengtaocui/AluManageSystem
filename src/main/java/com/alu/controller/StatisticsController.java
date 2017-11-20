d8d136cc871ddfdc0a6803fcb2ce6b35f8476177
mengtaocui <mengtao123>
2017-11-20 13:37:21 +0800

    1

---
+++ src/main/java/com/alu/controller/StatisticsController.java
@@ -0,0 +1,254 @@
+package com.alu.controller;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.apache.log4j.Logger;
+import org.jeecgframework.core.beanvalidator.BeanValidators;
+import org.jeecgframework.core.common.controller.BaseController;
+import org.jeecgframework.core.common.hibernate.qbc.CriteriaQuery;
+import org.jeecgframework.core.common.model.json.AjaxJson;
+import org.jeecgframework.core.common.model.json.DataGrid;
+import org.jeecgframework.core.constant.Globals;
+import org.jeecgframework.core.util.DateUtils;
+import org.jeecgframework.core.util.MyBeanUtils;
+import org.jeecgframework.core.util.ResourceUtil;
+import org.jeecgframework.core.util.StringUtil;
+import org.jeecgframework.tag.core.easyui.TagUtil;
+import org.jeecgframework.tag.vo.datatable.SortDirection;
+import org.jeecgframework.web.system.pojo.base.TSUser;
+import org.jeecgframework.web.system.service.SystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.alu.common.AuTools;
+import com.alu.common.Constant;
+import com.alu.entity.ActivityEntity;
+import com.alu.entity.ActivitySpaceEntity;
+import com.alu.entity.NoticesEntity;
+import com.alu.service.ActivityServiceI;
+
+/**   
+ * @Title: Controller
+ * @Description: 活动
+ * @author zhangdaihao
+ * @date 2017-11-07 17:16:35
+ * @version V1.0   
+ *
+ */
+@Controller
+@RequestMapping("/statisticsController")
+public class StatisticsController extends BaseController {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(StatisticsController.class);
+
+	@Autowired
+	private ActivityServiceI activityService;
+	@Autowired
+	private SystemService systemService;
+	@Autowired
+	private Validator validator;
+	
+
+
+	/**
+	 * 按毕业年届统计 页面跳转
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "statisticsByYearPeriodPage")
+	public ModelAndView statisticsByYearPeriodPage(HttpServletRequest request) {
+		return new ModelAndView("alu/statisticsByYearPeriod");
+	}
+
+	/**
+	 * 按教育阶段统计 页面跳转
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "statisticsByEducationStagePage")
+	public ModelAndView statisticsByEducationStagePage(HttpServletRequest request) {
+		return new ModelAndView("alu/statisticsByEducationStage");
+	}
+	
+	/**
+	 * 按省份统计 页面跳转
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "statisticsByProvincePage")
+	public ModelAndView statisticsByProvincePage(HttpServletRequest request) {
+		return new ModelAndView("alu/statisticsByProvince");
+	}
+	
+	/**
+	 * 按单位性质统计 页面跳转
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "statisticsByCompanyNaturePage")
+	public ModelAndView statisticsByCompanyNaturePage(HttpServletRequest request) {
+		return new ModelAndView("alu/statisticsByCompanyNature");
+	}
+	
+	/**
+	 * easyui AJAX请求数据
+	 * 
+	 * @param request
+	 * @param response
+	 * @param dataGrid
+	 * @param user
+	 */
+
+	@RequestMapping(params = "datagrid")
+	public void datagrid(ActivityEntity activity,HttpServletRequest request, HttpServletResponse response, DataGrid dataGrid) {
+		CriteriaQuery cq = new CriteriaQuery(ActivityEntity.class, dataGrid);
+		//查询条件组装器
+		activity.setName(AuTools.createLikeStr(activity.getName()));
+		activity.setPlaces(AuTools.createLikeStr(activity.getPlaces()));
+		
+		//报名截止时间
+		if(StringUtil.isNotEmpty(request.getParameter("applyEndTime_begin1"))){
+			cq.ge("applyEndTime", request.getParameter("applyEndTime_begin1"));
+		}
+		if(StringUtil.isNotEmpty(request.getParameter("applyEndTime_end2"))){
+			cq.le("applyEndTime", request.getParameter("applyEndTime_end2"));
+		}
+		
+		//开始时间
+		if(StringUtil.isNotEmpty(request.getParameter("startTime_begin1"))){
+			cq.ge("startTime", request.getParameter("startTime_begin1"));
+			cq.ge("endTime", request.getParameter("startTime_begin1"));
+		}
+		if(StringUtil.isNotEmpty(request.getParameter("startTime_end2"))){
+			cq.le("startTime", request.getParameter("startTime_end2"));
+			cq.le("endTime", request.getParameter("startTime_end2"));
+		}
+		
+		//降序排列
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("crtTime", SortDirection.desc);
+		cq.setOrder(map);
+		//过滤掉删除的
+		cq.eq("deleteFlag", Constant.UN_DELETE);
+		org.jeecgframework.core.extend.hqlsearch.HqlGenerateUtil.installHql(cq, activity, request.getParameterMap());
+		this.activityService.getDataGridReturn(cq, true);
+		TagUtil.datagrid(response, dataGrid);
+	}
+
+	/**
+	 * 删除活动
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "del")
+	@ResponseBody
+	public AjaxJson del(ActivityEntity activity, HttpServletRequest request) {
+		String message = null;
+		AjaxJson j = new AjaxJson();
+		activity = systemService.getEntity(ActivityEntity.class, activity.getId());
+		message = "活动删除成功";
+		activity.setDeleteFlag(Constant.IS_DELETE);
+		activityService.updateEntitie(activity);
+		systemService.addLog(message, Globals.Log_Type_DEL, Globals.Log_Leavel_INFO);
+		
+		j.setMsg(message);
+		return j;
+	}
+
+
+	/**
+	 * 添加活动
+	 * 
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping(params = "save")
+	@ResponseBody
+	public AjaxJson save(ActivityEntity activity, HttpServletRequest request) {
+		String message = null;
+		AjaxJson j = new AjaxJson();
+		TSUser curUser = ResourceUtil.getSessionUser();
+		if (StringUtil.isNotEmpty(activity.getId())) {
+			message = "活动更新成功";
+			ActivityEntity t = activityService.get(ActivityEntity.class, activity.getId());
+			try {
+				activity.setLastUpdateBy(curUser.getId());
+				activity.setLastUpdateByUserName(curUser.getUserName());
+				activity.setLastUpdateTime(DateUtils.formatDateTime());
+				MyBeanUtils.copyBeanNotNull2Bean(activity, t);
+				activityService.saveOrUpdate(t);
+				systemService.addLog(message, Globals.Log_Type_UPDATE, Globals.Log_Leavel_INFO);
+			} catch (Exception e) {
+				e.printStackTrace();
+				message = "活动更新失败";
+			}
+		} else {
+			message = "活动添加成功";
+			activity.setCrtBy(curUser.getId());
+			activity.setCrtByUserName(curUser.getUserName());
+			activity.setCrtTime(DateUtils.formatDateTime());
+			activity.setCheckStatus(Constant.APPLY_WAIT);
+			activity.setDeleteFlag(Constant.UN_DELETE);
+			activityService.save(activity);
+			systemService.addLog(message, Globals.Log_Type_INSERT, Globals.Log_Leavel_INFO);
+		}
+		j.setMsg(message);
+		return j;
+	}
+
+	/**
+	 * 活动列表页面跳转
+	 * 
+	 * @return
+	 */
+	@RequestMapping(params = "addorupdate")
+	public ModelAndView addorupdate(ActivityEntity activity, HttpServletRequest req) {
+		if (StringUtil.isNotEmpty(activity.getId())) {
+			activity = activityService.getEntity(ActivityEntity.class, activity.getId());
+			req.setAttribute("activityPage", activity);
+		}
+		return new ModelAndView("alu/activity");
+	}
+	
+	/**
+	 * 审核活动
+	 * @param id
+	 * @param type
+	 * @return
+	 */
+	@RequestMapping(params = "handleApply", method = RequestMethod.POST)
+	@ResponseBody
+	public AjaxJson handleApply(String id, Integer type) {
+		AjaxJson aj = new AjaxJson();
+		ActivityEntity activity = systemService.getEntity(ActivityEntity.class, id);
+		TSUser curUser = ResourceUtil.getSessionUser();
+		activity.setCheckBy(curUser.getId());
+		activity.setCheckByUserName(curUser.getUserName());
+		activity.setCheckStatus(type);
+		activityService.updateEntitie(activity);
+		systemService.addLog("审核活动："+activity.getName(), Globals.Log_Type_DEL, Globals.Log_Leavel_INFO);
+		return aj;
+	}
+}
---
+++ src/main/webapp/webpage/alu/statisticsByEducationStage.jsp
@@ -0,0 +1,103 @@
+<%@ page language="java" import="java.util.*" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@include file="/context/mytags.jsp"%>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>活动</title>
+  <t:base type="jquery,easyui,tools,DatePicker"></t:base>
+  <style type="text/css">
+  	input{
+  		width: 157px;
+  	}
+  </style>
+ </head>
+ <body style="overflow-y: hidden" scroll="no">
+  <t:formvalid formid="formobj" dialog="true" usePlugin="password" layout="table" action="activityController.do?save">
+			<input id="id" name="id" type="hidden" value="${activityPage.id }">
+			<table style="width: 100%;" cellpadding="0" cellspacing="1" class="formtable">
+				<tr>
+					<td align="right">
+						<label class="Validform_label">
+							活动名称:
+						</label>
+					</td>
+					<td class="value">
+						<input class="inputxt" id="name" name="name" datatype="*"  value="${activityPage.name}" />
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+				<tr>
+					<td align="right">
+						<label class="Validform_label">
+							活动地点:
+						</label>
+					</td>
+					<td class="value">
+						<input class="inputxt" id="places" name="places"   value="${activityPage.places}" />
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+				<tr>
+					<td align="right">
+						<label class="Validform_label">
+							开始时间:
+						</label>
+					</td>
+					<td class="value">
+						<input class="inputxt easyui-datetimebox" id="startTime" name="startTime" required="required"  value="${activityPage.startTime}" />
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+				<tr>
+					<td align="right">
+						<label class="Validform_label">
+							结束时间:
+						</label>
+					</td>
+					<td class="value">
+						<input class="inputxt easyui-datetimebox" id="endTime" name="endTime" required="required"  value="${activityPage.endTime}" />
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+				<tr>
+					<td align="right">
+						<label class="Validform_label">
+							报名截止时间:
+						</label>
+					</td>
+					<td class="value">
+						<input class="inputxt easyui-datetimebox" id="applyEndTime" name="applyEndTime" required="required"  value="${activityPage.applyEndTime}" />
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+				<tr style="height:260px">
+					<td align="right">
+						<label class="Validform_label">
+							活动详情:
+						</label>
+					</td>
+					<td class="value">
+						<!-- 加载编辑器的容器 -->
+					    <textarea id="contentDetail" name="contentDetail" cols="20" rows="2" class="ckeditor">${activityPage.detail}</textarea>
+						<input type="hidden" id="detail" onclick="OnSave()" name="detail"/>
+						<%-- <input class="inputxt" id="detail" name="detail" ignore="ignore"  value="${activityPage.detail}" /> --%>
+						<span class="Validform_checktip"></span>
+					</td>
+				</tr>
+			</table>
+		</t:formvalid>
+		<!-- 配置文件 -->
+	    <script type="text/javascript" src="plug-in/ckeditor/ckeditor.js"></script>
+	    <!-- 实例化编辑器 -->
+	    <script type="text/javascript">
+		    CKEDITOR.replace('contentDetail');
+			function OnSave(){  
+		        if(CKEDITOR.instances.contentDetail.getData()==""){  
+			        alert("内容不能为空！");  
+			        return false;  
+		        }else {  
+		        	$("#detail").val(CKEDITOR.instances.contentDetail.getData());
+		        }  
+		    } 
+		</script>
+ </body>
